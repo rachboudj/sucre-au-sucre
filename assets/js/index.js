d944c9d4e5cde415d22a75a54c0901e7dff65921
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-02-19 18:09:52 +0100

    ajout de l'évènement permettant l'apparition/disparition du menu burger

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
 const burgerButton = document.querySelector(".burger-button");
 const burgerMenu = document.querySelector(".burger-menu");
+// const ancreBurger = document.querySelector(".ancre-burger");
+
+// ancreBurger.addEventListener('click', () => {
+//     console.log('cliiiiique');
+//     burgerMenu.style.top = "-130vh";
+// })
 
 burgerButton.addEventListener('click', () => {
     console.log('ça clique');
-    burgerButton.classList.toggle("active");
     burgerMenu.classList.toggle("active");
 })
